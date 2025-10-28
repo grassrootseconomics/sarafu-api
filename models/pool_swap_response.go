c11060648d90bfbfd1d4370c316f67f765e5ce0e
Alfred Kamanda <alfredmwaik@gmail.com>
2025-10-28 11:04:12 +0300

    added the CreditSendLimitsResult type for the API responses

--- models/pool_swap_response.go
+++ models/pool_swap_response.go
@@ -20,3 +20,8 @@ type MaxLimitResult struct {
 type TokenInPoolResult struct {
 	CanSwapFrom bool `json:"canSwapFrom"`
 }
+
+type CreditSendLimitsResult struct {
+	MaxRAT string `json:"maxRAT"`
+	MaxSAT string `json:"maxSAT"`
+}
